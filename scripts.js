71955fd0b0a937c7665563af9921eeb77358959d
kvinik <kvinik400@gmail.com>
2021-07-26 11:15:10 +0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ button.addEventListener('click' , function(){
     document.querySelector('#firstName').value = firstNames[Math.round(Math.random() * 4)];
     document.querySelector('#lastName').value = lastNames [Math.round(Math.random() * 4)];
     document.querySelector('#phoneNumber').value = '+' + Math.round(Math.random() * 1000000000);;
-    document.querySelector('#email').value = Math.random().toString(36).substring(7) + '@email.com';
+    document.querySelector('#email').value = Math.random().toString(36).substring(1) + '@email.com';
     document.querySelector('#subject').value = subjects[Math.round(Math.random() * 4)];
     document.querySelector('#text').value = Math.random().toString(36).substring(7);
 
