2721d301c69f5ff69e6c480c7a55c209c1a302a7
kvinik <kvinik400@gmail.com>
2021-07-26 11:13:17 +0400

    done

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var firstNames=['Shalva','Lasha', 'Gela' , 'Mamuka' , 'Nugzari']
+var lastNames=['Kheoshvili','Afciauri' , 'Papashvili' , 'Sarashvili' , 'Gorgadze'] 
+var subjects=['Subject1','Subject2', 'Subject3' , 'Subject4' , 'Subjec5']
+
+let button = document.querySelector('button')
+button.addEventListener('click' , function(){
+    document.querySelector('#firstName').value = firstNames[Math.round(Math.random() * 4)];
+    document.querySelector('#lastName').value = lastNames [Math.round(Math.random() * 4)];
+    document.querySelector('#phoneNumber').value = '+' + Math.round(Math.random() * 1000000000);;
+    document.querySelector('#email').value = Math.random().toString(36).substring(7) + '@email.com';
+    document.querySelector('#subject').value = subjects[Math.round(Math.random() * 4)];
+    document.querySelector('#text').value = Math.random().toString(36).substring(7);
+
+})
